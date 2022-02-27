a6d83ec1715f0ba1c217c62ce43a0bed4acc5df9
tttza <25410554+tttza@users.noreply.github.com>
2022-02-27 20:13:06 +0900

    Update Badge

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
+lang1 = 'ja-JP'
+lang2 = 'en-US'
+
+
+
 function check_url_redirect(details) {
     var pattern, redirectUrl;
-    lang1 = 'ja-JP'
-    lang2 = 'en-US'
-
+    
     function redirect_if_match(query, target, details){
         try {
             pattern = new RegExp(query, 'ig');
@@ -23,3 +26,42 @@ function check_url_redirect(details) {
   }
 
 chrome.action.onClicked.addListener(check_url_redirect);
+
+
+function update_extention_staus(details){
+    console.log(details)
+    function is_match(query, details){
+        try {
+            pattern = new RegExp(query, 'ig');
+          } catch(err) {
+            //bad pattern
+            return false
+          }
+          match = details.url.match(pattern);
+          return match
+    }
+
+    console.log(details.url)
+    if (details.url == 'about:blank') { return }
+    if (is_match(lang1, details)){
+        chrome.action.setBadgeText(
+            {text: lang1, tabId: details.tabId},
+          )
+        console.log(lang1)
+    }
+    else if (is_match(lang2, details)){
+        chrome.action.setBadgeText(
+            {text: lang2, tabId: details.tabId},
+          )
+        console.log(lang2)
+    } else {
+        chrome.action.setBadgeText(
+            {text: "", tabId: details.tabId},
+          )
+    }
+}
+
+
+chrome.webNavigation.onDOMContentLoaded.addListener(
+    update_extention_staus   
+)
